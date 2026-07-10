89729bc5288e5b00842d219c2451e046de89ac48
techavi <tech.avi.co@gmail.com>
2026-07-10 12:16:44 +0530

    pushing....

--- src/App.jsx
+++ src/App.jsx
@@ -73,24 +73,33 @@ function App() {
 
        <br></br><br></br>
 
+
+   <button className="mx-2 px-1  bg-black text-fuchsia-400 w-50 h-15 cursor-pointer"
+       onClick={()=>setVideoId('gtHKKZJ9W-Q')}>useRef Hook by  Love Babbar</button> 
+
+
          <button className="mx-2 px-1  bg-black text-fuchsia-400 w-50 h-15 cursor-pointer"
        onClick={()=>setVideoId('MXs2qqWcGHo')}>useMemo Hook by  Love Babbar</button> 
 
           <button className="mx-2 px-1  bg-black text-fuchsia-400 w-50 h-15 cursor-pointer"
        onClick={()=>setVideoId('Z7VSgQ_y0dg')}>useContext Hook by  Love Babbar</button> 
 
+
+         <br></br><br></br>
+
            <button className="mx-2 px-1  bg-black text-fuchsia-400 w-50 h-15 cursor-pointer"
        onClick={()=>setVideoId('_NZ5kb68IpA')}>useEffect Hook by  Love Babbar</button>
 
-       <br></br><br></br>
-
-
+  
          <button className="mx-2  bg-black text-fuchsia-400 w-50 h-15 cursor-pointer"
        onClick={()=>setVideoId('Z4hN0WZbASk')}>Lifting State by Love Babbar</button>
 
 
         <button className="mx-2 px-1  bg-black text-fuchsia-400 w-50 h-25 cursor-pointer"
        onClick={()=>setVideoId('BBgSB2bLy60')}>Passing PROPS as Childrenand and Function by Love Babbar</button>
+
+           <br></br><br></br>
+       
 
  <button className="m-2 px-1  bg-black text-fuchsia-400 w-50 h-25 cursor-pointer"
        onClick={()=>setVideoId('3qGQ4dGA42o')}>Mastering NEXT JS by Patel</button>
